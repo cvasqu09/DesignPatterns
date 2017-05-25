6eaa727c8d4ccefb2455dad13ec342cc1d9dff6f
Chris.V <cvasqu09@gmail.com>
2017-05-25 16:17:44 -0500

    Purposely messed up WindowTestCases for TravisCI

--- src/test/java/tests/abstractfactory/WindowTestCases.java
+++ src/test/java/tests/abstractfactory/WindowTestCases.java
@@ -11,7 +11,7 @@ public class WindowTestCases {
 	@Test
 	public void WindowsWindow_renderBatch_firstItem() {
 		Window windows = new WindowsWindow(500, 200);
-		assertEquals(windows.getBatch().get(0), "Windows Wallpaper");
+		assertEquals(windows.getBatch().get(1), "Windows Wallpaper");
 	}
 	
 	@Test
